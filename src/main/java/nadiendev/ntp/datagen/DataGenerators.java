840e24e1459cfe2ad65e3e4c8d0ab5d138b62289
SrNadien 🔥 <76491773+SrNadien@users.noreply.github.com>
2026-03-10 12:27:12 -0300

    Bump Version: 10.0 (1.21.3)

--- src/main/java/nadiendev/ntp/datagen/DataGenerators.java
+++ src/main/java/nadiendev/ntp/datagen/DataGenerators.java
@@ -1,12 +1,22 @@
 package nadiendev.ntp.datagen;
 
 import nadiendev.ntp.datagen.providers.ModRecipeProvider;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
-public class DataGenerators {
-    private DataGenerators() {}
 
-    public static void gatherData(GatherDataEvent.Client event) {
-        event.createProvider(ModRecipeProvider.Runner::new);
+public class DataGenerators {
+    private DataGenerators() {}                         
+
+                                  
+    public static void gatherData(GatherDataEvent event) {
+        event.getGenerator().addProvider(              
+            event.includeServer(),                     
+            new ModRecipeProvider.Runner(              
+                event.getGenerator().getPackOutput(),  
+                event.getLookupProvider()              
+            )
+        );
     }
 }
